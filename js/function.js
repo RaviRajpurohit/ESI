bbffce2b8155bcaefbb2a2feba98fdbc77a06411
Ravi Rajpurohit <ravirajpurohit29@gmail.com>
2017-01-19 16:08:28 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -4,14 +4,16 @@
 function recieved_page(){
 	document.getElementById('mainIframe').src = 'other/recieved_page.php';
 }
-//------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------
-
 function issued_page(){
 	document.getElementById('mainIframe').src = 'other/issued_page.php';
 }
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
 
+function issued_other(){
+	document.getElementById('mainIframe').src = 'other/issued_other.php';
+}
 function print_values(){
 	document.getElementById('mainIframe').src = 'other/print_value.php';
 }
